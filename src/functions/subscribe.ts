eb7d297b516b9504a0cc6116cfbfb8d953dd6d38
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-24 14:41:08 -0300

    feat: register user and update reference ranking in Redis

--- src/functions/subscribe.ts
+++ src/functions/subscribe.ts
@@ -1,23 +1,42 @@
+import { eq } from 'drizzle-orm'
 import { db } from '../drizzle/client'
 import { subscriptions } from '../drizzle/tables/subscriptions'
+import { redis } from '../redis/client'
 
 interface Subscribe {
   name: string
   email: string
+  referral?: string | null
 }
 
-export async function subscribe({ name, email }: Subscribe) {
-  const result = await db
+export async function Subscribe({ name, email, referral }: Subscribe) {
+  const subscribers = await db
+    .select()
+    .from(subscriptions)
+    .where(eq(subscriptions.email, email))
+
+  if (subscribers.length > 0) {
+    return { subscriberId: subscribers[0].id }
+  }
+
+  const [{ subscriberId }] = await db
     .insert(subscriptions)
     .values({
       name,
       email,
     })
-    .returning()
+    .returning(
+      {
+        subscriberId: subscriptions.id,
+      }
+    )
 
-  const subscriber = result[0]
+  if (referral) {
+    await redis.zincrby('referral:ranking', 1, referral)
+  }
+
 
   return {
-    subscriberId: subscriber.id,
+    subscriberId
   }
 }
